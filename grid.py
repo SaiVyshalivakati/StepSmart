f25737ceb341841ec7a16f8bab75a6cf8fb6d1bb
SaiVyshalivakati <saivyshalivakati@gmail.com>
2026-04-26 21:11:07 -0400

    basegrid

---
+++ grid.py
@@ -0,0 +1,40 @@
+store = [
+    [1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,1,0,0,0,0,1],
+    [1,0,1,0,1,0,1,1,0,1],
+    [1,0,1,0,0,0,0,1,0,1],
+    [1,0,1,1,1,1,0,1,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,1,0,1,1,1,0,1,1,1],
+    [1,0,0,0,1,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1],
+]
+ 
+items={ "Vegetables":(4,4),
+         "Fruits":(2,9),
+         "Coffee":(6,6),
+         "Meat":(9,9),
+         "Bakery":(8,6)}
+
+def basegrid(store,items):
+    for i,line in enumerate(store):
+        for j,cell in enumerate(line):
+                itempos=None
+                for name,(r,c) in items.items():
+                    if r--i and c==j:
+                        itempos=name
+
+                if itempos:
+                    print("Item",end=" ")
+
+                elif cell==1:
+                    print("\u2588",end=" ")
+
+                else:
+                    print("*",end=" ")
+
+        print()
+                    
+
+basegrid(store,items)           
